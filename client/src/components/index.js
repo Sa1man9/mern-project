117ba57f530efcf4c59321ee04bc77b831805a04
Sa1man9 <salmanmohd781@gmail.com>
2023-07-29 18:46:20 +0530

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,21 @@
+import Logo from './Logo';
+import FormRow from './FormRow';
+import Alert from './Alert';
+import Navbar from './Navbar';
+import BigSidebar from './BigSidebar';
+import SmallSidebar from './SmallSidebar';
+import FormRowSelect from './FormRowSelect';
+import Job from './Job';
+import JobsContainer from './JobsContainer';
+import SearchContainer from'./SearchContainer';
+import JobInfo from './JobInfo';
+import Loading from './Loading';
+import ChartsContainer from './ChartsContainer';
+import StatsContainer from './StatsContainer';
+import StatItem from './StatItem';
+export { Logo, FormRow, Alert,Navbar,
+    BigSidebar,
+    SmallSidebar,FormRowSelect,JobsContainer,Job,SearchContainer,JobInfo,Loading,
+    StatItem,
+    StatsContainer,
+    ChartsContainer}
